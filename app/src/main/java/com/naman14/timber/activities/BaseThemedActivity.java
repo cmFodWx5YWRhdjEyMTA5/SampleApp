9773356a1791dec468d0a66f0cae10d59d920ce0
Shopify-1 <metizsoft@gmail.com>
2018-09-21 18:24:55 +0530

    my second commit

--- app/src/main/java/com/naman14/timber/activities/BaseThemedActivity.java
+++ app/src/main/java/com/naman14/timber/activities/BaseThemedActivity.java
@@ -23,5 +23,7 @@ public class BaseThemedActivity extends ATEActivity {
         super.onCreate(savedInstanceState);
         //make volume keys change multimedia volume even if music is not playing now
         setVolumeControlStream(AudioManager.STREAM_MUSIC);
+
+
     }
 }
